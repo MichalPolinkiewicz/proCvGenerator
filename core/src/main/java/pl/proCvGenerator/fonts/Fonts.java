c5fab85e4d090618344c9ae91e4417a22f923eac
MichalPolinkiewicz <michal.polinkiewicz@gmail.com>
2018-11-08 23:13:57 +0100

    add new fonts

--- core/src/main/java/pl/proCvGenerator/fonts/Fonts.java
+++ core/src/main/java/pl/proCvGenerator/fonts/Fonts.java
@@ -6,7 +6,14 @@ import com.itextpdf.text.pdf.BaseFont;
 
 public class Fonts {
 
-    public static final Font UBUNTU_NORMAL = FontFactory.getFont("fonts/Ubuntu-Light.ttf", BaseFont.IDENTITY_H, BaseFont.EMBEDDED, 10, Font.NORMAL);
-    public static final Font UBUNTU_BOLD = FontFactory.getFont("fonts/Ubuntu-Light.ttf", BaseFont.IDENTITY_H, BaseFont.EMBEDDED, 10, Font.BOLD);
+    public static final Font UBUNTU_NORMAL = FontFactory.getFont("fonts/ubuntu.ttf", BaseFont.IDENTITY_H, BaseFont.EMBEDDED, 10, Font.NORMAL);
+    public static final Font UBUNTU_BOLD = FontFactory.getFont("fonts/ubuntu.ttf", BaseFont.IDENTITY_H, BaseFont.EMBEDDED, 10, Font.BOLD);
+    public static final Font TAHOMA_NORMAL = FontFactory.getFont("fonts/tahoma.ttf", BaseFont.IDENTITY_H, BaseFont.EMBEDDED, 10, Font.NORMAL);
+    public static final Font TAHOMA_BOLD = FontFactory.getFont("fonts/tahoma.ttf", BaseFont.IDENTITY_H, BaseFont.EMBEDDED, 10, Font.BOLD);
+    public static final Font CALIBRI_NORMAL = FontFactory.getFont("fonts/calibri.ttf", BaseFont.IDENTITY_H, BaseFont.EMBEDDED, 10, Font.NORMAL);
+    public static final Font CALIBRI_BOLD = FontFactory.getFont("fonts/calibri.ttf", BaseFont.IDENTITY_H, BaseFont.EMBEDDED, 10, Font.BOLD);
+    public static final Font VERDANA_NORMAL = FontFactory.getFont("fonts/verdana.ttf", BaseFont.IDENTITY_H, BaseFont.EMBEDDED, 10, Font.NORMAL);
+    public static final Font VERDANA_BOLD = FontFactory.getFont("fonts/verdana.ttf", BaseFont.IDENTITY_H, BaseFont.EMBEDDED, 10, Font.BOLD);
+
 
 }
--- core/src/main/java/pl/proCvGenerator/patterns/PatternImpl2.java
+++ core/src/main/java/pl/proCvGenerator/patterns/PatternImpl2.java
@@ -3,14 +3,14 @@ package pl.proCvGenerator.patterns;
 import com.itextpdf.text.*;
 import com.itextpdf.text.pdf.PdfPCell;
 import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.draw.VerticalPositionMark;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import pl.proCvGenerator.dto.CvContent;
 import pl.proCvGenerator.fonts.Fonts;
 
+import javax.print.Doc;
 import java.io.IOException;
-import java.net.MalformedURLException;
-import java.nio.charset.MalformedInputException;
 
 public class PatternImpl2 implements Pattern {
 
@@ -31,40 +31,41 @@ public class PatternImpl2 implements Pattern {
 
     @Override
     public void generateCv(Document document, CvContent cvContent) {
-        createHeader(document);
-        createMainTable(document);
+//        createHeader(document);
+//        createMainTable(document);
+        test(document);
+        test2(document);
     }
 
     public void createHeader(Document document) {
-        Element element = new Rectangle(10, 680, 585, 832);
+        Element element = new Rectangle(10, 660, 585, 832);
         ((Rectangle) element).setBackgroundColor(BaseColor.BLUE);
 
         try {
             document.add(element);
-            Paragraph paragraph = createHeaderPharagraph("Jan Norlak", Fonts.UBUNTU_BOLD, 28);
+            Paragraph paragraph = createParagraph("Jan Norlak", Fonts.UBUNTU_BOLD, 28);
             paragraph.setAlignment(Element.ALIGN_CENTER);
             paragraph.setSpacingBefore(10);
-            paragraph.setSpacingAfter(20);
+            paragraph.setSpacingAfter(30);
             document.add(paragraph);
 
-            PdfPTable table = new PdfPTable(4);
-            table.setWidths(new int[]{1, 1, 1,1});
+            PdfPTable table = new PdfPTable(3);
+            table.setWidths(new int[]{1, 1, 1});
             table.setWidthPercentage(100);
 
-            PdfPCell cell = new PdfPCell(createHeaderPharagraph("xx", Fonts.UBUNTU_NORMAL, 16));
-            cell.setFixedHeight(50);
+            PdfPCell cell = new PdfPCell();
+            cell.setFixedHeight(60);
             cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
             cell.setHorizontalAlignment(Element.ALIGN_CENTER);
+
             Image image = Image.getInstance("/home/michal/IdeaProjects/proCvGenerator/core/src/main/resources/images/phone.png");
-            image.scalePercent(1.5f);
-            Paragraph paragraph1 = new Paragraph("xx");
-            paragraph1.setAlignment(Element.ALIGN_RIGHT);
-            cell.addElement(paragraph1);
-            image.setAlignment(Element.ALIGN_LEFT);
-            cell.addElement(image);
+            image.scalePercent(2.5f);
 
+            paragraph = new Paragraph(new Chunk(image, 0, -20));
+            paragraph.setAlignment(Element.ALIGN_LEFT);
+            paragraph.add(createChunk("michal.polinkiewicz@gmail.com", Fonts.UBUNTU_NORMAL, 12));
 
-
+            cell.addElement(paragraph);
             //cell.setBorder(Rectangle.NO_BORDER);
             table.addCell(cell);
             table.addCell(cell);
@@ -76,8 +77,7 @@ public class PatternImpl2 implements Pattern {
 
         } catch (DocumentException e) {
 
-        }
-        catch (IOException e){
+        } catch (IOException e) {
             System.out.println(e);
 
         }
@@ -93,7 +93,7 @@ public class PatternImpl2 implements Pattern {
             table.setWidthPercentage(100);
 
             PdfPCell cell = new PdfPCell();
-            paragraph = createHeaderPharagraph("this is header", Fonts.UBUNTU_BOLD, 18);
+            paragraph = createParagraph("this is header", Fonts.UBUNTU_BOLD, 18);
             cell.addElement(paragraph);
             cell.setFixedHeight(300);
 
@@ -113,7 +113,7 @@ public class PatternImpl2 implements Pattern {
 
     }
 
-    public Paragraph createHeaderPharagraph(String text, Font font, int fontSize) {
+    public Paragraph createParagraph(String text, Font font, int fontSize) {
         font.setSize(fontSize);
         Paragraph paragraph = new Paragraph(text, font);
         paragraph.setSpacingBefore(10);
@@ -121,5 +121,41 @@ public class PatternImpl2 implements Pattern {
 
         return paragraph;
     }
+
+    public Chunk createChunk(String text, Font font, int fontSize){
+        font.setSize(fontSize);
+
+        return new Chunk(text, font);
+    }
+
+    public void test(Document document){
+        try {
+            Rectangle rectangle = new Rectangle(0, 0, 200, 842);
+            rectangle.setBackgroundColor(BaseColor.CYAN);
+            document.add(rectangle);
+
+            rectangle = new Rectangle(201, 0, 595, 842);
+            rectangle.setBackgroundColor(BaseColor.DARK_GRAY);
+
+            document.add(rectangle);
+        } catch (DocumentException e){
+
+        }
+
+    }
+
+    public void test2(Document document){
+        Paragraph p = createParagraph("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", Fonts.CALIBRI_NORMAL, 14);
+        p.setIndentationRight(395);
+
+        try {
+
+            document.add(p);
+
+        } catch (DocumentException e){
+
+        }
+    }
+
 
 }
