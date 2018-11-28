bf1a7414bcea1186c2b08404d52c6e774a84de5e
Michal.Polinkiewicz <Michal.Polinkiewicz@empirica.pl>
2018-11-28 16:41:47 +0100

    pattern3 impl almost complete

--- core/src/main/java/pl/proCvGenerator/patterns/PatternImpl3.java
+++ core/src/main/java/pl/proCvGenerator/patterns/PatternImpl3.java
@@ -3,17 +3,39 @@ package pl.proCvGenerator.patterns;
 import com.itextpdf.text.*;
 import com.itextpdf.text.pdf.PdfPCell;
 import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.draw.LineSeparator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import pl.proCvGenerator.dto.CvContent;
+import pl.proCvGenerator.dto.Education;
+import pl.proCvGenerator.dto.Employment;
 import pl.proCvGenerator.dto.PersonalInfo;
+import pl.proCvGenerator.exception.TooMuchCharsException;
 import pl.proCvGenerator.fonts.Fonts;
 import pl.proCvGenerator.patterns.helpers.PatternHelper;
+import sun.awt.image.PixelConverter;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+import static pl.proCvGenerator.patterns.helpers.PatternHelper.*;
+import static pl.proCvGenerator.patterns.helpers.PatternHelper.validateEmployments;
+import static pl.proCvGenerator.patterns.helpers.PatternHelper.validateStringList;
 
 public class PatternImpl3 implements Pattern {
 
     public static final Logger LOGGER = LoggerFactory.getLogger(PatternImpl3.class);
     private static final String CLASS_NAME = PatternImpl3.class.getSimpleName();
+    private Font normalFont = Fonts.ANTONIO_NORMAL;
+    private Font boldFont = Fonts.ANTONIO_BOLD;
+    private static final int MAX_LINES_FOR_PAGE = 26;
+
+    //left side STRING max = 32
+    //right side = 80
+    //max liczba wierszy = 26
+    //extra przy employent = 27
+    //extra przy education = 20
 
     @Override
     public Document prepareDocument() {
@@ -26,19 +48,19 @@ public class PatternImpl3 implements Pattern {
     }
 
     @Override
-    public void generateCv(Document document, CvContent cvContent) {
+    public void generateCv(Document document, CvContent cvContent) throws TooMuchCharsException {
         createCvStructure(document);
         createCvBody(document, cvContent);
     }
 
     public void createCvStructure(Document document) {
         try {
-            Rectangle rectangle = new Rectangle(0, 0, 194, 842);
-            rectangle.setBackgroundColor(BaseColor.PINK);
+            Rectangle rectangle = new Rectangle(0, 0, 199, 842);
+            rectangle.setBackgroundColor(BaseColor.WHITE);
             document.add(rectangle);
 
-            rectangle = new Rectangle(197, 0, 595, 842);
-            rectangle.setBackgroundColor(BaseColor.CYAN);
+            rectangle = new Rectangle(202, 0, 595, 842);
+            rectangle.setBackgroundColor(BaseColor.WHITE);
             document.add(rectangle);
 
             rectangle = new Rectangle(0, 680, 595, 842);
@@ -50,90 +72,209 @@ public class PatternImpl3 implements Pattern {
         }
     }
 
-    public void createCvBody(Document document, CvContent cvContent) {
-        generateHeader(document, cvContent.getPersonalInfo());
+    public void createCvBody(Document document, CvContent cvContent) throws TooMuchCharsException {
+        addCvHeader(document, cvContent.getPersonalInfo());
+        //walidacje
+        //left side STRING max = 32
+        //right side = 80
+        //max liczba wierszy = 26
+        //extra przy employent = 27
+        //extra przy education = 20
+        // LEFT COLUMN VALIDATION
+
+        BigDecimal desc = validateString(cvContent.getPersonalInfo().getDescription(), 29);
+        System.out.println("Description: " + desc);
+        BigDecimal edu = validateEducationsList(cvContent.getEducationList(), 26, 20);
+        System.out.println("Education: " + edu );
+        BigDecimal hobby =  validateStringList(cvContent.getHobbies(),260);
+        System.out.println("Hobby: " + hobby);
+        List<String> list = new ArrayList<>();
+        list.add(cvContent.getPersonalInfo().getPhone());
+        list.add(cvContent.getPersonalInfo().getEmail());
+        list.add(cvContent.getPersonalInfo().getCity());
+        if (cvContent.getPersonalInfo().getPage() != null){
+            list.add(cvContent.getPersonalInfo().getPage());
+        }
+        BigDecimal info = validateStringList(list,25);
+        System.out.println("Info: " + info);
+        System.out.println("Result: " + desc.add(edu).add(hobby).add(info));
+
+
+//        //RIGHT COLUMN VALIDATION
+//        if (validateStringList(skills, MAX_CHARS_IN_LINE_FOR_STRING_LIST_RIGHT)
+//                .add(validateEmployments(employments, MAX_CHARS_IN_LINE_FOR_EMPLOYMENT, EXTRA_CHARS_FOR_EMPLOYMENT))
+//                .compareTo(maxLinesForPage) > 0) {
+//            LOGGER.error(CLASS_NAME + " - " + methodName + " - " + "ERROR: too much chars in RIGHT SIDE");
+//            throw new TooMuchCharsException("Too much chars in right section");
+//        }
 
         try {
             PdfPTable table = new PdfPTable(3);
-            table.setWidthPercentage(105);
-           table.setSpacingBefore(35);
+            table.setWidthPercentage(100);
+            table.setSpacingBefore(35);
             table.setWidths(new float[]{13, 2, 28});
 
-            PdfPCell leftCell = generateLeftSection(document, cvContent);
+            PdfPCell leftCell = createLeftSection(cvContent);
+            leftCell.setBorder(Rectangle.NO_BORDER);
             PdfPCell space = new PdfPCell(new Phrase(" "));
-
-            PdfPCell rightCell = new PdfPCell(new Phrase("bbb"));
+            space.setBorder(Rectangle.NO_BORDER);
+            space.addElement(new LineSeparator(660, 10, new BaseColor(60, 93, 93), 100, -330));
+            PdfPCell rightCell = createRightSection(cvContent);
+            rightCell.setBorder(Rectangle.NO_BORDER);
 
             table.addCell(leftCell);
             table.addCell(space);
             table.addCell(rightCell);
 
-
             document.add(table);
         } catch (DocumentException e) {
 
         }
-
     }
 
-    public void generateHeader(Document document, PersonalInfo personalInfo) {
+    private void addCvHeader(Document document, PersonalInfo personalInfo) {
 
         try {
-            Font font = Fonts.ANTONIO_BOLD;
-            font.setColor(60, 93, 93);
-
-            Paragraph p = PatternHelper.createSimpleParagraph(
-                    (personalInfo.getName() + " " + personalInfo.getSurname()).toUpperCase(),
-                    font,
-                    44
-            );
+            boldFont.setColor(60, 93, 93);
+            Paragraph p = createSimpleParagraph((personalInfo.getName() + " " + personalInfo.getSurname()).toUpperCase(), boldFont, 44);
             p.setAlignment(Element.ALIGN_CENTER);
             document.add(p);
 
-            font = Fonts.ANTONIO_NORMAL;
-            font.setColor(80, 124, 124);
-            p = PatternHelper.createSimpleParagraph(
-                    personalInfo.getPosition().toUpperCase(),
-                    font,
-                    24
-            );
+            normalFont.setColor(80, 124, 124);
+            p = createSimpleParagraph(personalInfo.getPosition().toUpperCase(), normalFont, 24);
             p.setAlignment(Element.ALIGN_JUSTIFIED_ALL);
             p.setIndentationRight(150);
             p.setIndentationLeft(150);
-            p.setSpacingAfter(15);
             document.add(p);
 
         } catch (DocumentException e) {
-            LOGGER.error(CLASS_NAME + "- generateHeader() - " + "ERROR: " + e);
+            LOGGER.error(CLASS_NAME + "- addCvHeader() - " + "ERROR: " + e);
         }
-
     }
 
-    public PdfPCell generateLeftSection(Document document, CvContent cvContent) {
+    private PdfPCell createLeftSection(CvContent cvContent) {
         PdfPCell cell = new PdfPCell();
+        int aligment = Element.ALIGN_RIGHT;
 
-        try {
-            PdfPTable table = new PdfPTable(2);
-            table.setWidthPercentage(100);
-            table.setWidths(new float[]{1, 5});
-            table.addCell("img");
-            table.addCell(cvContent.getPersonalInfo().getEmail());
-            table.addCell("img");
-            table.addCell("text");
-
-            cell.addElement(table);
-
-
-        } catch (DocumentException e) {
-
-        }
+        cell.addElement(addSectionHeader("kontakt", aligment));
+        cell.addElement(addPersonalInfoSection(cvContent.getPersonalInfo()));
+        cell.addElement(addSectionHeader("o mnie", aligment));
+        cell.addElement(addAboutMeSection(cvContent.getPersonalInfo().getDescription()));
+        cell.addElement(addSectionHeader("wykształcenie", aligment));
+        cell.addElement(addEducationSection(cvContent.getEducationList()));
+        cell.addElement(addSectionHeader("hobby", aligment));
+        cell.addElement(addSectionFromList(cvContent.getHobbies(), Element.ALIGN_RIGHT));
 
         return cell;
     }
 
-    public PdfPCell generateRightSection() {
-        return new PdfPCell();
+    private PdfPCell createRightSection(CvContent cvContent) {
+        PdfPCell cell = new PdfPCell();
+        int aligment = Element.ALIGN_LEFT;
+
+        cell.addElement(addSectionHeader("doświadczenie zawodowe", aligment));
+        cell.addElement(addEmploymentSection(cvContent.getEmployments()));
+        cell.addElement(addSectionHeader("umiejętności", aligment));
+        cell.addElement(addSectionFromList(cvContent.getSkills(), aligment));
+
+        return cell;
     }
 
+    private Paragraph addSectionHeader(String text, int aligment) {
+        String textWithExtraSpace = "";
+        for (int i = 0; i < text.length(); i++){
+            textWithExtraSpace += text.charAt(i) + " ";
+        }
+        boldFont.setColor(60, 93, 93);
+        Paragraph p = createSimpleParagraph(textWithExtraSpace.toUpperCase(), boldFont, 18);
+        p.setSpacingAfter(5);
+        p.setAlignment(aligment);
+
+        return p;
+    }
+
+    private Paragraph addPersonalInfoSection(PersonalInfo personalInfo) {
+        Paragraph p = new Paragraph();
+        int aligment = Element.ALIGN_RIGHT;
+
+        Paragraph helper = createSimpleParagraph(personalInfo.getPhone(), normalFont, 12);
+        helper.setAlignment(aligment);
+        p.add(helper);
+
+        helper = createSimpleParagraph(personalInfo.getEmail(), normalFont, 12);
+        helper.setAlignment(aligment);
+        p.add(helper);
+
+        helper = createSimpleParagraph(personalInfo.getCity(), normalFont, 12);
+        helper.setAlignment(aligment);
+        p.add(helper);
+
+        if (personalInfo.getPage() != null){
+            helper = createSimpleParagraph(personalInfo.getPage(), normalFont, 12);
+            helper.setAlignment(aligment);
+            p.add(helper);
+        }
+        p.setSpacingAfter(15);
+
+        return p;
+    }
+
+    private Paragraph addAboutMeSection(String text) {
+        Paragraph p = createSimpleParagraph(text, normalFont, 12);
+        p.setAlignment(Element.ALIGN_RIGHT);
+        p.setSpacingAfter(15);
+
+        return p;
+    }
+
+    private Paragraph addEducationSection(List<Education> educations) {
+        Paragraph mainParagraph = new Paragraph();
+        List<Education> sorted = PatternHelper.sortEducationList(educations);
+
+        for (int i = 0; i < sorted.size(); i++) {
+            Education e = sorted.get(i);
+            Paragraph helperParagraph;
+            if (i == sorted.size() - 1) {
+                helperParagraph = createSimpleParagraph(("- " + e.getSchoolName() + ", " + e.getStartDate() + " - " + e.getEndDate() + ", kierunek: " + e.getSubject() + ", " + e.getDegree() + "."), normalFont, 12);
+            } else {
+                helperParagraph = createSimpleParagraph(("- " + e.getSchoolName() + ", " + e.getStartDate() + " - " + e.getEndDate() + ", kierunek: " + e.getSubject() + ", " + e.getDegree() + ","), normalFont, 12);
+                helperParagraph.setSpacingAfter(5);
+            }
+            helperParagraph.setAlignment(Element.ALIGN_RIGHT);
+            mainParagraph.add(helperParagraph);
+            mainParagraph.setSpacingAfter(15);
+        }
+        return mainParagraph;
+    }
+
+    private Paragraph addSectionFromList(List<String> list, int aligment) {
+        Paragraph mainParagraph = new Paragraph();
+
+        for (int i = 0; i < list.size(); i++) {
+            Paragraph helperParagraph;
+            if (i == list.size() - 1) {
+                helperParagraph = createSimpleParagraph("- " + list.get(i) + ".", normalFont, 12);
+            } else {
+                helperParagraph = createSimpleParagraph("- " + list.get(i) + ",", normalFont, 12);
+            }
+            helperParagraph.setAlignment(aligment);
+            mainParagraph.add(helperParagraph);
+        }
+        return mainParagraph;
+    }
+
+    private Paragraph addEmploymentSection(List<Employment> employments){
+        Paragraph paragraph = new Paragraph();
+        List<Employment> sorted = PatternHelper.sortEmploymentList(employments);
+
+        for (int i = 0; i < sorted.size(); i++) {
+            Employment e = sorted.get(i);
+            paragraph.add(createSimpleParagraph(e.getPosition().toUpperCase()
+                    + ", " + e.getCompany() + ", " + e.getStartDate() + " - " + e.getEndDate() + ".", normalFont,12));
+            paragraph.add(createSimpleParagraph("Zakres obowiązków: " + e.getJobDescription() + ".", normalFont, 12));
+            paragraph.add(new Paragraph(" "));
+        }
+        return paragraph;
+    }
 
 }
--- core/src/main/java/pl/proCvGenerator/pdf/PdfCreator.java
+++ core/src/main/java/pl/proCvGenerator/pdf/PdfCreator.java
@@ -25,7 +25,7 @@ public class PdfCreator {
 
         try {
             Document document = pattern.prepareDocument();
-            PdfWriter.getInstance(document, new FileOutputStream(new File(PATH_TO_FILE + "12.pdf")));
+            PdfWriter.getInstance(document, new FileOutputStream(new File(PATH_TO_FILE_WINDOWS + "12.pdf")));
             document.open();
             pattern.generateCv(document, createUser().getCvContent());
             document.close();
@@ -68,7 +68,7 @@ public class PdfCreator {
         Employment zzz = new Employment();
         zzz.setCompany("zzz sp.z o.o");
         zzz.setPosition("murarz");
-        zzz.setJobDescription("Praca przy budowien nnvnvnb nowoczesnej fabryki mokrej karmy. Nadzór nad pracownikami, wykonywanie prac związanych z wykończeniem wnętrz, spozywanie alkoholu w duzych ilościach. Kradzież materiałów budowlanych i sprzedaż na czarnym rynku");
+        zzz.setJobDescription("Pracaxxxxxxxxxxxxxxxxxxxxxxx przy budowien nnvnvnb nowoczesnej fabryki mokrej karmy. Nadzór nad pracownikami, wykonywanie prac związanych z wykończeniem wnętrz, spozywanie alkoholu w duzych ilościach. Kradzież materiałów budowlanych i sprzedaż na czarnym rynku");
         zzz.setStartDate("2010");
         zzz.setEndDate("2017");
 
@@ -82,40 +82,38 @@ public class PdfCreator {
         personalInfo.setName("Bogusław");
         personalInfo.setSurname("Norlak");
         personalInfo.setCity("Jawor"); //max 19
-        personalInfo.setDescription("Jestem dobrym murarzem. xxxxxxxxxx. Lubie pic. Robie to od dziecka i jestem niesamowitym fachowcem. Ponadto jestem Andrzejem i bogdanem. Jestem dobrym murarzem. Lubie pic i murowac. Robie to od dziecka i jestem niesamowitym fachowcem. Ponadto jestem Andrzejem i bogdanem.");
+        personalInfo.setDescription("Jestem dobrym murarzem. Lubie pić.swwsswwswsw  Robie wsswswswsw wswswswswwsw to od dziecka i jestem niesamowitym fachowcem. Ponadto jestem Andrzejem i bogdanem. Jestem dobrym murarzem. Lubie pic i murowac. Robie to od dziecka i jestem niesamowitym fachowcem. Ponadto jestem Andrzejem i bogdanem.");
         personalInfo.setEmail("ada.nowak@wp.pl"); //max 30 znakow
         personalInfo.setPhone("645-093-582"); //max 9
         personalInfo.setPosition("stróż i alkoholik");
+        personalInfo.setPage("github.com/SnorliTheDog");
 
         cvContent.setPersonalInfo(personalInfo);
 
         List<String> hobbyList = new ArrayList<>();
         hobbyList.add("muzyka");
         hobbyList.add("sport");
+        hobbyList.add("inne");
+        hobbyList.add("inne");
         hobbyList.add("turystyka");
-        hobbyList.add("mocny alkohol tttt");
-        hobbyList.add("inne");
-        hobbyList.add("inne");
+        hobbyList.add("turystyka");
 
 
         cvContent.setHobbies(hobbyList);
         user.setCvContent(cvContent);
 
         List<String> skills = new ArrayList<>();
-//        skills.add("prawo jazdy kat. B");
-//        skills.add("umiejętność obsługi komputera xxxx xxxx xxxxxxxx xxxxxxxx");
-//        skills.add("umiejętność obsługi komputera xxxxxxxx xxx");
-//        skills.add("umiejętność obsługi komputera xxxxxxxx xxx");
-//        skills.add("umiejętność obsługi komputera xxxxxxxx xxx");
-//        skills.add("umiejętność obsługi komputera xxxxxxxxxx xxxxxxxxx");
-//        skills.add("umiejętność obsługi komputera xxxxxxxx xxxx");
-//        skills.add("umiejętność obsługi komputera xxx xxxxx xxxxxx xxxxxx");
-//        skills.add("umiejętność obsługi komputera xxx xxxxx xxxxxx xxxxxx");
-//        skills.add("umiejętność obsługi komputera xxx xxxxx xxxxxx xxxxxx");
+        skills.add("prawo jazdy kat. B");
+        skills.add("umiejętność obsługi komputera xxxx xxxx xxxxxxxx xxxxxxxx");
+        skills.add("umiejętność obsługi komputera xxxxxxxx xxx");
+        skills.add("umiejętność obsługi komputera xxxxxxxx xxx");
+        skills.add("umiejętność obsługi komputera xxxxxxxx xxx");
+        skills.add("umiejętność obsługi komputera xxxxxxxxxx xxxxxxxxx");
+        skills.add("umiejętność obsługi komputera xxxxxxxx xxxx");
+        skills.add("umiejętność obsługi komputera xxx xxxxx xxxxxx xxxxxx");
+        skills.add("umiejętność obsługi komputera xxx xxxxx xxxxxx xxxxxx");
+        skills.add("umiejętność obsługi komputera xxx xxxxx xxxxxx xxxxxx");
 
-        for (int i = 0; i < 13 ; i++){
-            skills.add("skill " + i);
-        }
 
 
         cvContent.setSkills(skills);
